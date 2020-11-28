a9841fe99714a475e4e044ed8e7eb78d4161b51d
Yoav <yoavklein25@gmail.com>
2020-11-28 20:08:44 +0200

    netns script

--- system_programming/network/tcp/netns.sh
+++ system_programming/network/tcp/netns.sh
@@ -15,9 +15,9 @@ function setup() {
 	echo "set veth-blue to blue namespace"
 	sudo ip link set veth-blue netns blue
 	echo "assign ip address to veth-red"
-	sudo ip netns exec red ip addr add 192.168.15.1 dev veth-red
+	sudo ip netns exec red ip addr add 192.168.15.1/24 dev veth-red
 	echo "assign ip address to veth-blue"
-	sudo ip netns exec blue ip addr add 192.168.15.2 dev veth-blue
+	sudo ip netns exec blue ip addr add 192.168.15.2/24 dev veth-blue
 	echo "veth-red up"
 	sudo ip netns exec red ip link set veth-red up
 	echo "veth-blue up"
@@ -25,4 +25,35 @@ function setup() {
 
 }
 
-
+function setup_bridge()
+{
+	echo "create bridge interface"
+	sudo ip link add bridge type bridge
+	echo "bring bridge up"
+	sudo ip link set dev bridge up
+	echo "create veth-pair for red namespace"
+	sudo ip link add veth-red type veth peer name veth-red-br
+	echo "create veth-pair for blue namespace"
+	sudo ip link add veth-blue type veth peer name veth-blue-br
+	echo "set the veth-red interface to red namespace"
+	sudo ip link set veth-red netns red
+	echo "set the veth-red-br to the bridge"
+	sudo ip link set veth-red-br master bridge
+	echo "set the veth-blue interface to blue namespace"
+	sudo ip link set veth-blue netns blue
+	echo "set the veth-blue-be to the bridge"
+	sudo ip link set veth-blue-br master bridge
+	echo "assign ip for red interface"
+	sudo ip netns exec red ip addr add 192.168.15.1/24 dev veth-red
+	echo "assing ip for blue interface"
+	sudo ip netns exec blue ip addr add 192.168.15.2/24 dev veth-blue
+	echo "veth-red up"
+	sudo ip netns exec red ip link set veth-red up
+	echo "veth-red-br up"
+	sudo ip link set veth-red-br up
+	echo "veth-blue up"
+	sudo ip netns exec blue ip link set veth-blue up
+	echo "veth-blue-br up"
+	sudo ip link set veth-blue-br up
+	
+}
